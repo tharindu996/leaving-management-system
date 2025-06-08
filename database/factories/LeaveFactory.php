76334c139ad5a3845162f2b2e747014ced120179
tharindu996 <tharindudissanayake03@gmail.com>
2025-06-08 22:02:37 +0530

    create factory for leaves

---
+++ database/factories/LeaveFactory.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\leave>
+ */
+class LeaveFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'description' => $this->faker->sentence(),
+            'leave_date' => $this->faker->dateBetween('today', '+1 month'),
+            'is_approved' => $this->faker->boolean(50), // 50% chance of being approved
+        ];
+    }
+}
