e024f1e524be537657c0b6e7dbc2dfbb2b10a10a
tharindu996 <tharindudissanayake03@gmail.com>
2025-06-08 15:30:14 +0530

    create admin views

---
+++ resources/js/pages/Admin/Leaves/Index.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h2>leave</h2>
+    </div>
+</template>
+
+<script setup>
+</script>
---
+++ resources/js/pages/Admin/Leaves/Show.vue
@@ -0,0 +1,8 @@
+<template>
+    <div>
+        <h2>leave-class</h2>
+    </div>
+</template>
+
+<script setup>
+</script>
