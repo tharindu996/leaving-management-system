d32d276a2140a986f9651c11532c092635e2a80d
tharindu996 <tharindudissanayake03@gmail.com>
2025-06-08 15:00:41 +0530

    add admin details to seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,7 +17,9 @@ class DatabaseSeeder extends Seeder
 
         User::factory()->create([
             'name' => 'Test User',
-            'email' => 'test@example.com',
+            'role' => 'admin',
+            'email' => 'admin@example.com',
+            'password' => 'password',
         ]);
     }
 }
