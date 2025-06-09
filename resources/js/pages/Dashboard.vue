f1c93e029a355823d7b470df36ee218464399172
tharindu996 <tharindudissanayake03@gmail.com>
2025-06-09 19:21:57 +0530

    display user stats

--- resources/js/pages/Dashboard.vue
+++ resources/js/pages/Dashboard.vue
@@ -2,8 +2,13 @@
 import AppLayout from '@/layouts/AppLayout.vue';
 import { type BreadcrumbItem } from '@/types';
 import { Head } from '@inertiajs/vue3';
-import PlaceholderPattern from '../components/PlaceholderPattern.vue';
 
+defineProps({
+    totalLeaves: Number,
+    approvedLeaves: Number,
+    pendingLeaves: Number,
+    rejectedLeaves: Number,
+});
 const breadcrumbs: BreadcrumbItem[] = [
     {
         title: 'Dashboard',
@@ -18,19 +23,31 @@ const breadcrumbs: BreadcrumbItem[] = [
     <AppLayout :breadcrumbs="breadcrumbs">
         <div class="flex h-full flex-1 flex-col gap-4 rounded-xl p-4">
             <div class="grid auto-rows-min gap-4 md:grid-cols-3">
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
+                <div class="aspect-video rounded-lg border-1">
+                    <div class="flex h-full flex-col items-center justify-center p-4">
+                        <div class="box-value text-4xl font-bold">{{ totalLeaves }}</div>
+                        <div class="box-title text-md mt-3">Total leaves</div>
+                    </div>
                 </div>
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
+                <div class="aspect-video rounded-lg border-1">
+                    <div class="flex h-full flex-col items-center justify-center p-4">
+                        <div class="box-value text-4xl font-bold">{{ pendingLeaves }}</div>
+                        <div class="box-title text-md mt-3">Pending leaves</div>
+                    </div>
                 </div>
-                <div class="relative aspect-video overflow-hidden rounded-xl border border-sidebar-border/70 dark:border-sidebar-border">
-                    <PlaceholderPattern />
+                <div class="aspect-video rounded-lg border-1">
+                    <div class="flex h-full flex-col items-center justify-center p-4">
+                        <div class="box-value text-4xl font-bold">{{ approvedLeaves }}</div>
+                        <div class="box-title text-md mt-3">Approved leaves</div>
+                    </div>
+                </div>                
+                <div class="aspect-video rounded-lg border-1">
+                    <div class="flex h-full flex-col items-center justify-center p-4">
+                        <div class="box-value text-4xl font-bold">{{ rejectedLeaves }}</div>
+                        <div class="box-title text-md mt-3">Rejected leaves</div>
+                    </div>
                 </div>
-            </div>
-            <div class="relative min-h-[100vh] flex-1 rounded-xl border border-sidebar-border/70 md:min-h-min dark:border-sidebar-border">
-                <PlaceholderPattern />
-            </div>
+            </div>          
         </div>
     </AppLayout>
 </template>
