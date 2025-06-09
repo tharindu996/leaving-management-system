85a4a1d3e3bde1afb4bbfca58a07bcf46678ac52
tharindu996 <tharindudissanayake03@gmail.com>
2025-06-09 11:54:41 +0530

    add user relatioship to leave model

--- app/Models/Leave.php
+++ app/Models/Leave.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\SoftDeletes;
 
 class Leave extends Model
@@ -20,4 +21,10 @@ class Leave extends Model
         'leave_date' => 'date',
         'is_approved' => 'boolean',
     ];
+
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
 }
