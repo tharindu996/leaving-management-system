254faf2402a0842428a1c8633392c74e5edcff92
tharindu996 <tharindudissanayake03@gmail.com>
2025-06-09 14:21:54 +0530

    update sidebar

--- resources/js/components/UserMenuContent.vue
+++ resources/js/components/UserMenuContent.vue
@@ -22,15 +22,7 @@ defineProps<Props>();
             <UserInfo :user="user" :show-email="true" />
         </div>
     </DropdownMenuLabel>
-    <DropdownMenuSeparator />
-    <DropdownMenuGroup>
-        <DropdownMenuItem :as-child="true">
-            <Link class="block w-full" :href="route('profile.edit')" prefetch as="button">
-                <Settings class="mr-2 h-4 w-4" />
-                Settings
-            </Link>
-        </DropdownMenuItem>
-    </DropdownMenuGroup>
+    <DropdownMenuSeparator />    
     <DropdownMenuSeparator />
     <DropdownMenuItem :as-child="true">
         <Link class="block w-full" method="post" :href="route('logout')" @click="handleLogout" as="button">
